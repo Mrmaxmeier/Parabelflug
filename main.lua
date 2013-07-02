e36b3ac95641ced0fc351c65ce17b6f18109b447
Mrmaxmeier <Mrmaxmeier@gmail.com>
2013-07-02 17:58:21 +0200

    scrolledPixel

--- main.lua
+++ main.lua
@@ -3,17 +3,17 @@ function love.load()
 	mid = love.graphics.newImage("assets/Mid.png")
 	back = love.graphics.newImage("assets/Back.png")
 	images = {{img=back, speed=50}, {img=mid, speed=100},{img=front, speed=200}}
-	n = 0
+	scrolledPixel = 0
 end
 
 function love.update(dt)
-	n = n + dt
+	scrolledPixel = scrolledPixel + dt
 end
 
 function love.draw()
 	for i, v in ipairs(images) do
 		w = v.img:getWidth()
-		love.graphics.draw(v.img, -n*v.speed%w)
-		love.graphics.draw(v.img, -n*v.speed%w - w)
+		love.graphics.draw(v.img, -scrolledPixel*v.speed%w)
+		love.graphics.draw(v.img, -scrolledPixel*v.speed%w - w)
 	end
 end
