292b98487e0c2c241cecd1314e1eea9610db23ef
felix <felixbauckholt AT gmail PUNKT com>
2013-07-02 18:34:50 +0200

    FLUUUGZEUGE!!!!!!!!!!

--- main.lua
+++ main.lua
@@ -1,13 +1,17 @@
 function love.load()
+	require "player"
+	
 	front = love.graphics.newImage("assets/Front.png")
 	mid = love.graphics.newImage("assets/Mid.png")
 	back = love.graphics.newImage("assets/Back.png")
 	images = {{img=back, speed=50}, {img=mid, speed=100},{img=front, speed=200}}
 	scrolledPixel = 0
+	player = Player:new(love.graphics.newImage("assets/Plane_Side.png"), 300, 200)
 end
 
 function love.update(dt)
 	scrolledPixel = scrolledPixel + dt
+	player:update(dt)
 end
 
 function love.draw()
@@ -16,4 +20,5 @@ function love.draw()
 		love.graphics.draw(v.img, -scrolledPixel*v.speed%w)
 		love.graphics.draw(v.img, -scrolledPixel*v.speed%w - w)
 	end
+	player:draw()
 end
---
+++ player.lua
@@ -0,0 +1,27 @@
+require "class"
+
+Player = class:new()
+
+function Player:init(img, x, y)
+	self.img = img
+	self.x = x
+	self.y = y
+	self.acc = 100
+	self.slope = 0.002
+	self.dy = 0
+end
+
+function Player:update(dt)
+	self.y = self.y + self.dy * dt
+	self.dy = self.dy * 0.8^dt
+	if love.keyboard.isDown("down") then
+		self.dy = self.dy + dt*self.acc
+	end
+	if love.keyboard.isDown("up") then
+		self.dy = self.dy - dt*self.acc
+	end
+end
+
+function Player:draw()
+	love.graphics.draw(self.img, self.x, self.y, self.dy*self.slope, 1, 1, 300, 70)
+end
